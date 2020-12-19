3699467af39dad0472ab953b709d4bd028b48ffb
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2020-12-19 18:56:05 +0100

    Move api to utils

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ module.exports = {
     ConfigFile: require("./configFile"),
     FSUtils: require("./fsUtils"),
     CryptoUtils: require("./cryptoUtils")
+    ApiServer: require("./api/server"),
+    ApiRequest: require("./api/request"),
+    ApiResponse: require("./api/response"),
+    ApiModel: require("./api/model")
 };
